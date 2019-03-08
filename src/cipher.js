e15f9ff123574825ad57c776872b9e4bd0cbf8ac
DeysiFernandez <deysi_forever_04@hotmail.com>
2019-03-08 08:42:22 -0500

    cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,51 @@
+
+
 window.cipher = {
-  encode: () => {
+  encode: (offset,string) => {
     /* Acá va tu código */
+    let newMessage="";
+    for(let i=0; i<string.length; i++){
+      if(string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90){
+        let numberAscii=((((string[i].charCodeAt())-65)+offset)%26+65);
+        let letterCipher=String.fromCharCode(numberAscii);
+        newMessage=newMessage+letterCipher;
+
+      } else if(string[i].charCodeAt()>=97 && string[i].charCodeAt()<=122){
+        let numberAscii=((((string[i].charCodeAt())-97)+offset)%26+97);
+        let letterCipher=String.fromCharCode(numberAscii);
+        newMessage=newMessage+letterCipher;
+
+      }
+      else{
+        newMessage=newMessage+string[i];
+      }
+
+    }
+    return newMessage;
+
   },
-  decode: () => {
+  decode: (string,offset) => {
     /* Acá va tu código */
-  }
+    let newMessage="";
+    for(let i=0; i<string.length; i++){
+      if(string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90){
+        let numberAscii=((((string[i].charCodeAt())+65)-offset)%26+65);
+        console.log(numberAscii);
+        let letterCipher=String.fromCharCode(numberAscii);
+        newMessage=newMessage+letterCipher;
+
+      } else if(string[i].charCodeAt()>=97 && string[i].charCodeAt()<=122){
+        let numberAscii=((((string[i].charCodeAt())-97)-offset)%26+97);
+        let letterCipher=String.fromCharCode(numberAscii);
+        newMessage=newMessage+letterCipher;
+
+      }
+      else{
+        newMessage=newMessage+string[i];
+      }
+
+    }
+    return newMessage;
+  
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 /* Acá va tu código */
+const message=document.getElementById("message");
+const number=document.getElementById("displace");
+const buttonCipher=document.getElementById("button");
+const result=document.getElementById("result");
+const buttonDecipher=document.getElementById("button2");
+const resultDecipher=document.getElementById("resultDecipher");
+
+const clickCipher=()=>{
+    let writtenMessage=message.value;
+    let position=parseInt(number.value);
+    let newMessage=cipher.encode(position,writtenMessage);
+    return result.value=newMessage;
+}
+buttonCipher.addEventListener("click",clickCipher);
+
+const clickDecipher=()=>{
+      let writtenMessage= result.value;
+      let position=parseInt(number.value);
+      let renewMessage = cipher.decode(writtenMessage,position);
+      console.log(renewMessage)
+      resultDecipher.innerHTML=renewMessage;
+}
+buttonDecipher.addEventListener("click",clickDecipher);
